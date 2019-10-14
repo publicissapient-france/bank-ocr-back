c5bda604fce78f0ff5ac4b03791ba3a3dab3e336
Peter Onneby <ponneby@xebia.fr>
2019-10-14 23:05:08 +0200

    Allow any request origins

--- bankocr.service/Startup.cs
+++ bankocr.service/Startup.cs
@@ -20,11 +20,21 @@ namespace bankocr.service
             Configuration = configuration;
         }
 
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+                {
+                    options.AddPolicy(MyAllowSpecificOrigins,
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin();
+                    });
+                });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -40,6 +50,8 @@ namespace bankocr.service
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseCors(MyAllowSpecificOrigins); 
 
             app.UseHttpsRedirection();
             app.UseMvc();
